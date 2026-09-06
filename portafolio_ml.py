9af03c66b60b7511a7ad640f50ff5718f412ccb0
estefaniamunoz801-ai <estefania.munoz801@pascualbravo.edu.co>
2026-09-06 16:30:17 -0500

    refactor Streamlit app layout to enhance visual structure and image sizing

--- portafolio_ml.py
+++ portafolio_ml.py
@@ -16,12 +16,12 @@ url_ml = "https://sites.google.com/view/aplicacionesdeia/inicio"
 st.subheader("En el siguiente enlace puedes encontrar páginas y ejercicios prácticos")
 st.write(f"Enlace para páginas y ejercicios: [Enlace]({url_ml})")
 
-col1, col2= st.columns(2, gap="large")
+col1, col2, col3 = st.columns(3, gap="large")
 
 with col1:
     st.subheader("Regresión Lineal")
     image = Image.open("imag/regresion_lineal.png")
-    st.image(image, width=210)
+    st.image(image, width=250)
     st.write("En el siguiente enlace veremos cómo usar la regresión lineal para predecir datos.")
     url = "https://predictor-en-vivo.streamlit.app/"
     st.write(f"Regresión Lineal: [Enlace]({url})")
@@ -29,27 +29,25 @@ with col1:
 with col2:
     st.subheader("Regresión Logística")
     image = Image.open("imag/regresion_logistica.png")
-    st.image(image, width=210)
+    st.image(image, width=250)
     st.write("En el siguiente enlace veremos cómo usar la regresión logística para clasificar datos.")
     url = "https://app-seguros-python.streamlit.app/"
     st.write(f"Regresión Logística: [Enlace]({url})")
 
-col3, col4 = st.columns(2, gap="large") 
-
 with col3:
     st.subheader("Clasificación con KNN")
     image = Image.open("imag/knn_clasificacion.png")
-    st.image(image, width=210)
+    st.image(image, width=250)
     st.write("En el siguiente enlace veremos cómo clasificar datos usando el algoritmo KNN.")
     url = "https://ml-clasificacion-k-means-svm.streamlit.app/"
     st.write(f"KNN: [Enlace]({url})")
 
-col5, col6 = st.columns(2, gap="large")
+col4, col5, col6 = st.columns(3, gap="large")
 
 with col4:
     st.subheader("Agrupamiento con K-Means")
     image = Image.open("imag/kmeans_consumo_electrico_v2.png")
-    st.image(image, width=210)
+    st.image(image, width=250)
     st.write("En el siguiente enlace veremos cómo agrupar datos usando K-Means en una app de consumo electrico.")
     url = "https://app-consumo-ml.streamlit.app/"
     st.write(f"K-Means: [Enlace]({url})")
@@ -58,7 +56,7 @@ with col5:
     st.subheader("Árboles de Decisión")
 
     image = Image.open("imag/arboles_decision_v2.png")
-    st.image(image, width=210)
+    st.image(image, width=250)
     st.write("En el siguiente enlace veremos cómo funciona un árbol de decisión.")
     url = "https://sistema-de-riego-inteligente-ml-arboles-decision.streamlit.app/"
     st.write(f"Árbol de Decisión: [Enlace]({url})")
@@ -67,7 +65,7 @@ with col6:
     st.subheader("Medidor de energia con ML")
 
     image = Image.open("imag/consumo_electrico_influxdb.png")
-    st.image(image, width=210)
+    st.image(image, width=250)
     st.write("En el siguiente enlace veremos un dashboard interactivo para análisis y modelado con Machine Learning de datos de consumo eléctrico almacenados en InfluxDB.")
     url = "https://medidor-energia-ml.streamlit.app/"
     st.write(f"Medidor de Energía con ML: [Enlace]({url})")
